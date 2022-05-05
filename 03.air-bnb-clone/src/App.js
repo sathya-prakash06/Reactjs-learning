9a9d36888dbf836bc20530a60cd38a115716a5f6
Sathya <sathyaprakash.sp.6@gmail.com>
2022-05-05 18:25:36 +0530

    airBnb clone updated

--- 03.air-bnb-clone/src/App.js
+++ 03.air-bnb-clone/src/App.js
@@ -2,39 +2,7 @@ import "./App.css";
 import Card from "./components/Card";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
-import one from "./assets/1.png";
-import two from "./assets/2.jpg";
-import three from "./assets/3.jpg";
-
-const card = [
-  {
-    img: one,
-    rating: "5.0",
-    reviews: "6",
-    country: "USA",
-    price: "$136",
-    title: "Life lessons with Katie Zaferes",
-    openSpot: "0",
-  },
-  {
-    img: two,
-    rating: "4.0",
-    reviews: "30",
-    country: "USA",
-    price: "$125",
-    title: "Learn wedding photography",
-    openSpot: "2",
-  },
-  {
-    img: three,
-    rating: "4.8",
-    reviews: "2",
-    country: "USA",
-    price: "$50",
-    title: "Group Mountain Biking",
-    openSpot: "1",
-  },
-];
+import { cardData } from "./data";
 
 function App() {
   return (
@@ -42,8 +10,8 @@ function App() {
       <Navbar />
       <HeroSection />
       <div className="cards-grid">
-        {card.map((item, index) => {
-          return <Card {...item} key={index}/>;
+        {cardData.map((item, index) => {
+          return <Card {...item} key={index} />;
         })}
       </div>
     </div>
---
+++ 03.air-bnb-clone/src/data.js
@@ -0,0 +1,36 @@
+import one from "./assets/1.png";
+import two from "./assets/2.jpg";
+import three from "./assets/3.jpg";
+
+export const cardData = [
+  {
+    id: 1,
+    img: one,
+    rating: "5.0",
+    reviews: "6",
+    country: "USA",
+    price: "$136",
+    title: "Life lessons with Katie Zaferes",
+    openSpot: "0",
+  },
+  {
+    id: 2,
+    img: two,
+    rating: "4.0",
+    reviews: "30",
+    country: "USA",
+    price: "$125",
+    title: "Learn wedding photography",
+    openSpot: "2",
+  },
+  {
+    id: 3,
+    img: three,
+    rating: "4.8",
+    reviews: "2",
+    country: "USA",
+    price: "$50",
+    title: "Group Mountain Biking",
+    openSpot: "1",
+  },
+];
