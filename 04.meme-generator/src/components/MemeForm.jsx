2aa0416ab78520dd18d4b5bd279f581d5e26160a
Sathya <sathyaprakash.sp.6@gmail.com>
2022-05-06 09:35:43 +0530

    memes data added

--- 04.meme-generator/src/components/MemeForm.jsx
+++ 04.meme-generator/src/components/MemeForm.jsx
@@ -1,7 +1,16 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./memeForm.css";
+import data from "../jsonData/memes.json";
 
 const MemeForm = () => {
+  const [memes, setMemes] = useState("");
+
+  const handleClick = () => {
+    console.log("clicked");
+    let randomIndex = Math.floor(Math.random() * data.data.memes.length);
+    let memes = data.data.memes[randomIndex].url;
+    setMemes(memes);
+  };
   return (
     <main>
       <form action="">
@@ -9,10 +18,12 @@ const MemeForm = () => {
           <input type="text" placeholder="Top text" />
           <input type="text" placeholder="Bottom text" />
         </div>
-        <div className="meme-btn">
-          <button type="submit">Get a new meme image</button>
-        </div>
       </form>
+      <div className="meme-btn">
+        <button onClick={handleClick}>Get a new meme image</button>
+      </div>
+
+      <div>{memes && <img src={memes} alt="memes_img" />}</div>
     </main>
   );
 };
